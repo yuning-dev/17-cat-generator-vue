dd125c91a8c762b687011971cbc4fe476a46b5b5
yuning-dev <yuning.eng@gmail.com>
2024-02-01 12:16:29 +0000

    get cat id from route params

--- src/pages/CatDetails/CatDetails.vue
+++ src/pages/CatDetails/CatDetails.vue
@@ -1,8 +1,16 @@
 <template>
-    view cat
+    view cat {{ catId }}
 </template>
 
 <script>
+export default {
+    name: 'CatDetails',
+    computed: {
+        catId() {
+            return this.$route.params.id
+        }
+    }
+}
 </script>
 
 <style module src="./CatDetails.css" />
